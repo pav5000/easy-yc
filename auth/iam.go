586ddc35bb0d20a7ae649de5fc58fe1b3e867831
asmyasnikov <79263256394@ya.ru>
2022-03-26 17:59:48 +0300

    revert ydbCredentials

--- auth/iam.go
+++ auth/iam.go
@@ -6,46 +6,26 @@ import (
 	"strings"
 
 	ycsdk "github.com/yandex-cloud/go-sdk"
-	"github.com/ydb-platform/ydb-go-sdk/v3/credentials"
 )
 
-type iamCredentials struct {
-	ycCreds ycsdk.NonExchangeableCredentials
-}
-
-func (i iamCredentials) YandexCloudAPICredentials() {
-}
-
-func (i iamCredentials) Token(ctx context.Context) (string, error) {
-	tokenRes, err := creds.ycCreds.IAMToken(ctx)
-	if err != nil {
-		return "", err
-	}
-	return tokenRes.IamToken, nil
-}
-
-var creds *iamCredentials
+var creds ycsdk.NonExchangeableCredentials
 
 func init() {
 	data, err := ioutil.ReadFile("iam.txt")
 	if err == nil {
 		token := strings.TrimSpace(string(data))
 		if token != "" {
-			creds = &iamCredentials{
-				ycsdk.InstanceServiceAccount(),
-			}
+			creds = ycsdk.NewIAMTokenCredentials(token)
 			return
 		}
 	}
-	creds = &iamCredentials{
-		ycsdk.InstanceServiceAccount(),
-	}
-}
-
-func GetYdbCredentials() credentials.Credentials {
-	return creds
+	creds = ycsdk.InstanceServiceAccount()
 }
 
 func GetIAMToken(ctx context.Context) (string, error) {
-	return creds.Token(ctx)
+	tokenRes, err := creds.IAMToken(ctx)
+	if err != nil {
+		return "", err
+	}
+	return tokenRes.IamToken, nil
 }
