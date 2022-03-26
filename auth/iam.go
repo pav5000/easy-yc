18da1feb60f126b35cc7158fa14a5eabab32fd7d
asmyasnikov <79263256394@ya.ru>
2022-03-26 17:11:33 +0300

    fix ydb driver usage

--- auth/iam.go
+++ auth/iam.go
@@ -6,26 +6,46 @@ import (
 	"strings"
 
 	ycsdk "github.com/yandex-cloud/go-sdk"
+	"github.com/ydb-platform/ydb-go-sdk/v3/credentials"
 )
 
-var creds ycsdk.NonExchangeableCredentials
+type iamCredentials struct {
+	ycCreds ycsdk.NonExchangeableCredentials
+}
+
+func (i iamCredentials) YandexCloudAPICredentials() {
+}
+
+func (i iamCredentials) Token(ctx context.Context) (string, error) {
+	tokenRes, err := creds.ycCreds.IAMToken(ctx)
+	if err != nil {
+		return "", err
+	}
+	return tokenRes.IamToken, nil
+}
+
+var creds *iamCredentials
 
 func init() {
 	data, err := ioutil.ReadFile("iam.txt")
 	if err == nil {
 		token := strings.TrimSpace(string(data))
 		if token != "" {
-			creds = ycsdk.NewIAMTokenCredentials(token)
+			creds = &iamCredentials{
+				ycsdk.InstanceServiceAccount(),
+			}
 			return
 		}
 	}
-	creds = ycsdk.InstanceServiceAccount()
+	creds = &iamCredentials{
+		ycsdk.InstanceServiceAccount(),
+	}
+}
+
+func GetYdbCredentials() credentials.Credentials {
+	return creds
 }
 
 func GetIAMToken(ctx context.Context) (string, error) {
-	tokenRes, err := creds.IAMToken(ctx)
-	if err != nil {
-		return "", err
-	}
-	return tokenRes.IamToken, nil
+	return creds.Token(ctx)
 }
